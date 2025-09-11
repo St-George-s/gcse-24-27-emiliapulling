880b7f181f035ad8fa9c17e8348f796134af76fc
emiliapulling <pul001@stge.org.uk>
2025-09-11 11:03:16 +0000

    Conditional Loops

---
+++ Loops/conditionalLoops.py
@@ -0,0 +1,30 @@
+# Question 11
+# code = "rzy"
+# userInput = input("Enter code: ")
+# while code != userInput:
+#     print("NO")
+#     userInput = input("Please re-enter code: ")
+# print("Code accepted")
+
+# Question 12
+# code = "4003"
+# userInput = input("Enter code: ")
+# while code != userInput:
+#     print("NO")
+#     userInput = input("Please re-enter code: ")
+# print("Code accepted")
+
+# Question 13
+# userInput = int(input("Enter age: "))
+# while userInput <= 14:
+#     print("NO")
+#     userInput = int(input("Please re-enter age: "))
+# print("Age accepted")
+
+# Question 14
+# password = input("Enter password: ")
+# while len(password) < 5:
+#     password = input("Password not long enough, please re-enter: ")
+# print("Password accepted")
+
+# Question 15
--- Loops/fixedLoops.py
+++ Loops/fixedLoops.py
@@ -1,54 +1,60 @@
-for counter in range(10):
-    print("Hello World")
+# for counter in range(10):
+#     print("Hello World")
 
-# Question 1
-for counter in range(10):
-    print("Emilia")
+# # Question 1
+# for counter in range(10):
+#     print("Emilia")
 
-# Question 2
-name = input("Enter your name: ")
-for counter in range(1000):
-    print(name)
+# # Question 2
+# name = input("Enter your name: ")
+# for counter in range(1000):
+#     print(name)
 
-# Question 3
-name = input("Enter your name: ")
-for counter in range(1000):
-    print("Hello", name)
+# # Question 3
+# name = input("Enter your name: ")
+# for counter in range(1000):
+#     print("Hello", name)
 
-#Question 4
-for counter in range(1,13):
-    print(counter * 8)
+# #Question 4
+# for counter in range(1,13):
+#     print(counter * 8)
 
-# Question 5
-for counter in range(1,1001):
-    print(counter * 8)
+# # Question 5
+# for counter in range(1,1001):
+#     print(counter * 8)
 
-# Question 6
-for counter in range(1,13):
-    print("7 x", counter, "=" , 7 * counter)
+# # Question 6
+# for counter in range(1,13):
+#     print("7 x", counter, "=" , 7 * counter)
 
-# Question 7
-for counter in range(10):
-    age = input("Enter an age: ")
-    print(age)
+# # Question 7
+# for counter in range(10):
+#     age = input("Enter an age: ")
+#     print(age)
 
-#Question 8
-for counter in range(10):
-    age = int(input("Enter an age: "))
-    print(age + 10)
+# #Question 8
+# for counter in range(10):
+#     age = int(input("Enter an age: "))
+#     print(age + 10)
 
-#Question 9
-total = 0
-for counter in range(10):
-    age = int(input("Enter an age: "))
-    print(age)
-    total = total + age
-    print(total)
+# #Question 9
+# total = 0
+# for counter in range(10):
+#     age = int(input("Enter an age: "))
+#     print(age)
+#     total = total + age
+#     print(total)
     
-#Question 10
-for counter1 in range(1,13):
-    print("")
-    print(counter1, "times table")
-    for counter2 in range(1,13):
-        print(counter1 , "x", counter2 , "=" , counter1 * counter2)
-    
+# #Question 10
+# for counter1 in range(1,13):
+#     print("")
+#     print(counter1, "times table")
+#     for counter2 in range(1,13):
+#         print(counter1 , "x", counter2 , "=" , counter1 * counter2)
+
+# Extension Activity
+timesTable = int(input("Which times table?: "))
+howFar = int(input("How far?: "))
+
+for counter in range(1,howFar + 1):
+    print(counter * timesTable)
