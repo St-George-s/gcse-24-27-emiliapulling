2180ea8e7a8bb09f266b04f2f28fc6c9f45724b7
emiliapulling <pul001@stge.org.uk>
2025-10-02 11:03:53 +0000

    hello

--- logic.py
+++ logic.py
@@ -24,12 +24,49 @@
 #     print("That's a lot of apples!")
 
 # Question 4
-age = int(input("How old are you? "))
-driversLicense = input("Do you have a drivers license? ")
-if age >= 18 and driversLicense == "Yes":
-    print("You are eligible to drive!")
-else:
-    print("You can't drive.")
+# age = int(input("How old are you? "))
+# driversLicense = input("Do you have a drivers license? ")
+# if age >= 18 and driversLicense == "Yes":
+#     print("You are eligible to drive!")
+# else:
+#     print("You can't drive.")
 
-# Question 5
-speed =
+# # Question 5
+# speed = 60
+# is_raining = False
+# if speed < 60 or not is_raining:
+#     print("The condition is satisfied")
+# else:
+#     print("The condition is not satisfied")
+
+# # Question 6
+# study = int(input("How many hours have you studied for? "))
+# prepared = input("Do you feel prepared? ")
+# if study > 5 and prepared == "yes":
+#     print("Good for you")
+# else:
+#     print("Work harder")
+
+# # Question 8
+# savings = float(input("HOw much money do you have? "))
+# price = float(input("How much does the item cost? "))
+# if price < savings:
+#     print("You can buy it!")
+# else:
+#     print("You can't afford it")
+
+# # Question 7
+# assignments_completed = 6 
+# assignments_pending = 1 
+# if assignments_completed > 5 and assignments_pending < 2: 
+#     print("The assignment criteria are met.") 
+# else: 
+#     print("The assignment criteria are not met.")
+
+# # Question 9 
+# is_sunny = True 
+# is_weekend = True 
+# if (is_sunny and is_weekend) or (not is_sunny and not is_weekend):
+#     print("The conditions are met")
+# else:
+#     print("The conditions are not met")
