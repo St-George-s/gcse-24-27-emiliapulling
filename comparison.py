553fa608eb852701115e50d37c4025068b990630
emiliapulling <pul001@stge.org.uk>
2025-10-06 16:52:51 +0000

    hello

--- comparison.py
+++ comparison.py
@@ -1,1 +1,43 @@
-number1 = float(input("Choose a number"))
+# Question 1 
+# number1 = float(input("Choose a number"))
+# number2 = float(input("Choose another number"))
+# if number1 == number2:
+#     print("they are the same")
+# else:
+#     print("they are not the same")
+
+# # Question 2
+# number1 = float(input("Choose a number"))
+# number2 = float(input("Choose another number"))
+# if number1 > number2:
+#     print("the bigger number is", number1)
+# if number2 > number1:
+#     print("the bigger number is", number2)
+
+# # Question 3 
+# dividend = int(input("Enter the dividend: "))
+# divisor = int(input("Enter the divisor: "))
+# remainder = dividend % divisor
+# print("The remainder is", remainder)
+
+# # Question 4 
+# number = int(input("Enter a number: "))
+# if number > 0:
+#     print("The number is positive.")
+# elif number < 0:
+#     print("The number is negative.")
+# else:
+#     print("The number is zero.")
+
+# # Question 5 
+# length = float(input("Enter the length of the rectangle: "))
+# width = float(input("Enter the width of the rectangle: "))
+# print(length * width)
+
+# Question 6
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+if num1 % num2 == 0:
+    print(num1 , "is a multiple of", num2)
+else:
+    print(num1 , "is not a multiple of", num2)
