5e7bfc12a58b793f21e91128fa2b8377f353f51c
emiliapulling <pul001@stge.org.uk>
2025-10-06 16:53:15 +0000

    hello

--- comparison.py
+++ comparison.py
@@ -34,10 +34,10 @@
 # width = float(input("Enter the width of the rectangle: "))
 # print(length * width)
 
-# Question 6
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-if num1 % num2 == 0:
-    print(num1 , "is a multiple of", num2)
-else:
-    print(num1 , "is not a multiple of", num2)
+# # Question 6
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
+# if num1 % num2 == 0:
+#     print(num1 , "is a multiple of", num2)
+# else:
+#     print(num1 , "is not a multiple of", num2)
