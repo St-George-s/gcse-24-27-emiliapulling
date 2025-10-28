6b7d33aa0b43c36bc2951828751085ea44f1b87b
emiliapulling <pul001@stge.org.uk>
2025-10-28 10:26:10 +0000

    hello

---
+++ file handling/append_file.py
@@ -0,0 +1,2 @@
+with open("/workspaces/gcse-24-27-emiliapulling/file handling/holiday.txt", "a") as file:
+    file.write("\nWe played Uno")
---
+++ file handling/create_file.py
@@ -0,0 +1,2 @@
+with open("holiday.txt", "w") as file:
+    file.write("I went to Birmingham to see my grandparents\n")
