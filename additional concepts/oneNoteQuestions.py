321a1f2142589281601d71e370f65e456ca96bfb
emiliapulling <pul001@stge.org.uk>
2025-11-04 10:05:59 +0000

    hi

---
+++ additional concepts/oneNoteQuestions.py
@@ -0,0 +1,39 @@
+# Write a Python program to extract the first and last characters of a string entered by the user. 
+# userInput = input("What is your favourite colour?: ")
+# print(userInput[0])
+# print(userInput[-1])
+
+# Write a Python program to reverse a string entered by the user. 
+# userString = input("Enter a sentence: ")
+# reversedString = ""
+# for character in userString:
+#     reversedString = character + reversedString
+# print(reversedString)
+
+# Write a Python program to check if a string is a palindrome (reads the same forward and backward). 
+
+
+# Write a Python program to count the number of vowels in a string. 
+
+
+# Write a Python program to replace all spaces in a string with underscores (_). 
+
+ 
+
+# Generate and print one random integer from 1 to 6. 
+import random 
+
+myRandomNumber = random.randint(1, 6)
+print(myRandomNumber)
+
+# Roll two dice (1–6) and print both values and their total using + string concatenation. 
+import random 
+
+diceOne = random.randint(1, 6)
+diceTwo = random.randint(1, 6)
+print(str(diceOne) + "+" + str(diceTwo) + "=")
+print(str(diceOne + diceTwo))
+
+# Given the list below, use a random integer to select a valid index and print the item at that index. 
+
+# animals = ["otter", "red squirrel", "pine marten", "seal", "oystercatcher"]
