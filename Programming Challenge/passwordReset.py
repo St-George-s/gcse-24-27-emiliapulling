446ec9a30fac84ba09d212b0d90dae6eb516821b
emiliapulling <pul001@stge.org.uk>
2026-01-20 10:21:48 +0000

    hi

---
+++ Programming Challenge/passwordReset.py
@@ -0,0 +1,5 @@
+password1 = input("Enter a password: ")
+while password1(len) < 8:
+    print("Password too short")
+    password1 = input("Enter a password: ")
+if ord (letter) >= 65 and ord (letter) <= 90:
