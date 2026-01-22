b872b749e346b7acbfe6cc9fbb497fe51112e1ed
emiliapulling <pul001@stge.org.uk>
2026-01-22 12:04:16 +0000

    Coding Challenge

--- Programming Challenge/passwordReset.py
+++ Programming Challenge/passwordReset.py
@@ -1,5 +1,30 @@
 password1 = input("Enter a password: ")
-while password1(len) < 8:
-    print("Password too short")
-    password1 = input("Enter a password: ")
-if ord (letter) >= 65 and ord (letter) <= 90:
+# while len(password1) < 8:
+#     print("Password too short")
+#     password1 = input("Enter a password: ")
+
+capitalCount = 0
+lowerCaseCount = 0
+
+    
+while capitalCount == 0:
+    for letter in password1:
+        if ord (letter) >= 65 and ord (letter) <= 90:
+            capitalCount = capitalCount + 1
+    if capitalCount == 0:
+        print("You need a capital letter, re-enter your password.")
+        password1 = input("Enter a password: ")
+
+while lowerCaseCount == 0:
+    for letter in password1:
+        if ord (letter) >= 97 and ord (letter) <= 122:
+            lowerCaseCount = lowerCaseCount + 1
+    if lowerCaseCount == 0:
+        print("You need a lower case letter, re-enter your password.")
+        password1 = input("Enter a password: ")
+
+reenterPassword = input("Please enter your password again: ")
+if reenterPassword == password1:
+    print("Password reset!")
+else:
+    print("That isn't the same password, try again.")
