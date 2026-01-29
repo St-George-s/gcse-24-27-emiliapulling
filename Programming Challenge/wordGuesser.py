2b54b743adc1e1beb78d87d89cf7772bad79cf50
emiliapulling <pul001@stge.org.uk>
2026-01-29 12:04:12 +0000

    started word guesser

---
+++ Programming Challenge/wordGuesser.py
@@ -0,0 +1,7 @@
+import random
+lettersGuessed = []
+words = ["apple", "banana", "kiwi", "pineapple", "mango", "coconut"]
+randNum = random.randint(0,5)
+selectedWord = words[randNum]
+
+userLetter = ""
