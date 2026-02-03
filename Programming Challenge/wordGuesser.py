73499007b673f7c765b62be4fa605da5d5f001b4
emiliapulling <pul001@stge.org.uk>
2026-02-03 09:30:41 +0000

    wordGuesser finished

--- Programming Challenge/wordGuesser.py
+++ Programming Challenge/wordGuesser.py
@@ -5,3 +5,19 @@ randNum = random.randint(0,5)
 selectedWord = words[randNum]
 
 userLetter = ""
+lettersGuessed.append(userLetter)
+
+blankWord = ""
+for x in range (len(selectedWord)):
+    blankWord = blankWord + "_"
+
+while selectedWord != blankWord:
+    userLetter = input("Enter a letter: ")
+    found = False
+    for i in range(len(selectedWord)):
+        if selectedWord[i] == userLetter:
+            found = True
+            blankWord = blankWord[:i] + userLetter + blankWord[i + 1:]
+    print(blankWord)
+
+print("WELL DONE YOU GUESSED THE WORD!!!")
