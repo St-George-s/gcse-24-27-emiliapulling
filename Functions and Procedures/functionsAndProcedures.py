70c44e4bb5269ab153931965f3fe3dec6ff2f61a
emiliapulling <pul001@stge.org.uk>
2026-06-09 09:25:45 +0000

    functions and procedures

---
+++ Functions and Procedures/functionsAndProcedures.py
@@ -0,0 +1,21 @@
+# Procedure with one parameter
+def printName(name):
+    print(name)
+
+# Function with one parameter
+def printNameFunc(name):
+    return name
+
+# Call the procedure
+printName("Liyana")
+
+# Call the function and print returned value
+print(printNameFunc("Viggy"))
+
+# Call the function an store returned value in a variable
+returnedName = printNameFunc("Alina")
+print(returnedName)
+
+# Question 4
+def sphere_volume(radius):
+    volume = 4/3 * 3.14 * radius
